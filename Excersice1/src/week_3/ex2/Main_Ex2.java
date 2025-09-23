c100fbd12b1aa346081e9c1688d9ae5f8d4adb09
cr7_dn <nguyenxuanhuan.dev@gmail.com>
2025-09-23 07:44:35 +0700

    week3_final

--- Excersice1/src/week_3/ex2/Main_Ex2.java
+++ Excersice1/src/week_3/ex2/Main_Ex2.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package week_3_ex2;
+package week_3.ex2;
 
 import Utils.Validator;
 
